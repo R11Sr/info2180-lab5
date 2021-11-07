cee5c90291e06eed7648c7139c55d1b438ef6f55
r11Sr <rs_burner2@outlook.com>
2021-11-06 20:21:29 -0500

    completed exercise 5 only have some minor issues to fix

--- world.js
+++ world.js
@@ -1,10 +1,34 @@
 window.addEventListener('load',(event)=>{
     const LOOKUP_BUTTON = $("#lookup");
+    const LOOKUP_CITIES_BUTTON = $("#lookup-cities");
+
 
     LOOKUP_BUTTON.on("click", e=>{
         let Fdata = {
             country: $("#country").val()
         };
+        $.ajax({
+            type: "GET",
+            url: "world.php",
+            data: Fdata,
+            dataType: "html"
+        }).done(response =>{
+            $("#result").empty();
+            $("#result").append(response);
+            console.log("fetched look-up");
+        }).fail(()=>{
+                $("#result").empty();
+                $("#result").append("<h3> Failed to fetch data from the server.</h3>");
+                }
+            )
+            e.preventDefault();
+    });
+
+    LOOKUP_CITIES_BUTTON.on("click",e=>{
+        let Fdata = {
+            country: $("#country").val(),
+            context: "cities"} // This to distingush this button. 
+
         console.log(Fdata);
         $.ajax({
             type: "GET",
@@ -14,12 +38,14 @@ window.addEventListener('load',(event)=>{
         }).done(response =>{
             $("#result").empty();
             $("#result").append(response);
+            console.log("fetched look-up + cites");
         }).fail(()=>{
                 $("#result").empty();
                 $("#result").append("<h3> Failed to fetch data from the server.</h3>");
                 }
             )
-            e.preventDefault();
+        e.preventDefault();
+
     });
 
 });
--- world.php
+++ world.php
@@ -1,5 +1,6 @@
 <?php 
-  $country = filter_var($_GET['country'], FILTER_SANITIZE_STRING)
+  $country = filter_var($_GET['country'], FILTER_SANITIZE_STRING);
+  $cities = filter_var($_GET['context'], FILTER_SANITIZE_STRING);
 
 ?>
 
@@ -12,28 +13,54 @@ $password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
-// $stmt = $conn->query("SELECT * FROM countries WHERE name = \"{$country}\"");
+$stmt = $conn->query(" SELECT cities.name as city_name, cities.country_code, cities.district,
+cities.population,countries.name as country_name, countries.continent,countries.independence_year,countries.head_of_state FROM cities join countries on
+cities.country_code = countries.code WHERE countries.name LIKE '%$country%';");
+
+// $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
+
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-
-<table>
-  <tr>
-    <th>Country Name</th>
-    <th>Continent</th>
-    <th>Independence Year</th>
-    <th>Head of State</th>
-  </tr>
-
-  <?php foreach ($results as $row): ?>
+<?php if(empty($cities)): ?>
+  <?=$cities?>
+  <table>
     <tr>
-      <td><?=$row['name']?></td>
-      <td><?=$row['continent']?></td>
-      <td><?=$row['independence_year']?></td>
-      <td><?=$row['head_of_state']?></td>
+      <th>Country Name</th>
+      <th>Continent</th>
+      <th>Independence Year</th>
+      <th>Head of State</th>
     </tr>
 
-  <?php endforeach; ?>
-</table>
+    <?php foreach ($results as $row): ?>
+      <tr>
+        <td><?=$row['country_name']?></td>
+        <td><?=$row['continent']?></td>
+        <td><?=$row['independence_year']?></td>
+        <td><?=$row['head_of_state']?></td>
+      </tr>
+
+    <?php endforeach; ?>
+  </table>
+
+<?php else: ?>
+  <table>
+    <tr>
+      <th>Country Name</th>
+      <th>District</th>
+      <th>Population</th>
+    </tr>
+
+    <?php foreach ($results as $row): ?>
+      <tr>
+        <td><?=$row['city_name']?></td>
+        <td><?=$row['district']?></td>
+        <td><?=$row['population']?></td>
+
+      </tr>
+
+    <?php endforeach; ?>
+  </table>
+
+<?php endif;?>
